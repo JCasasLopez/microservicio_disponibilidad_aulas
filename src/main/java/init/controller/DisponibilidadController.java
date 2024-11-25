fc15deccb13f3e741e63895b943a23bb66a4da31
Jorge Casas <j.casas.lopez.26@gmail.com>
2024-11-25 18:13:43 +0100

    Nueva refactorización Controller: aulasDisponibles() + Controller: crearHorarioAula

--- src/main/java/init/controller/DisponibilidadController.java
+++ src/main/java/init/controller/DisponibilidadController.java
@@ -1,10 +1,10 @@
 package init.controller;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,13 +13,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import init.exceptions.BadRequestException;
-import init.exceptions.InternalServerException;
 import init.model.AulaDto;
+import init.model.SlotDto;
 import init.service.DisponibilidadService;
 
 @CrossOrigin("*")
 @RestController
 public class DisponibilidadController {
+	
+	@Value("${horario.apertura}")
+    private int horaInicio;
+	
 	DisponibilidadService disponibilidadService;
 	
 	public DisponibilidadController(DisponibilidadService disponibilidadService) {
@@ -29,27 +33,37 @@ public class DisponibilidadController {
 
 	@GetMapping(value="aulasDisponibles", produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<AulaDto>> aulasDisponibles(
-										        @RequestParam LocalDateTime horaInicio,
-										        @RequestParam LocalDateTime horaFin,
-										        @RequestParam(defaultValue = "0") int capacidad,
-										        @RequestParam(defaultValue = "false") boolean proyector,
-										        @RequestParam(defaultValue = "false") boolean altavoces){
-		try {
-			
-			if(horaInicio.isAfter(horaFin)) {
-	            throw new BadRequestException("La hora de inicio debe ser anterior a la hora de fin");
-			}
-			
-			if (capacidad < 0) {
-	            throw new BadRequestException("La capacidad no puede ser menor de 0");
-	        }
-			
-			List<AulaDto> aulasDisponibles = disponibilidadService.aulasDisponibles(horaInicio, horaFin, 
-																	capacidad, proyector, altavoces);
-			return new ResponseEntity<>(aulasDisponibles, HttpStatus.OK);  
-			
-	    } catch (Exception ex) {
-	        throw new InternalServerException("Error al acceder a la base de datos", ex);
-	    }
+			@RequestParam LocalDateTime horaInicio,
+			@RequestParam LocalDateTime horaFin,
+			@RequestParam(defaultValue = "0") int capacidad,
+			@RequestParam(defaultValue = "false") boolean proyector,
+			@RequestParam(defaultValue = "false") boolean altavoces){
+
+		if(horaInicio.isAfter(horaFin)) {
+			throw new BadRequestException("La hora de inicio debe ser anterior a la hora de fin");
+		}
+
+		if (capacidad < 0) {
+			throw new BadRequestException("La capacidad no puede ser menor de 0");
+		}
+
+		List<AulaDto> aulasDisponibles = disponibilidadService.aulasDisponibles(horaInicio, horaFin, 
+				capacidad, proyector, altavoces);
+		return ResponseEntity.ok(aulasDisponibles); 
+
 	}
+	
+	@GetMapping(value="crearHorarioAula", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<SlotDto>> crearHorarioAula(@RequestParam int idAula,
+			@RequestParam LocalDateTime inicioSemana){
+		
+		if(inicioSemana.getDayOfWeek() != DayOfWeek.MONDAY || inicioSemana.getHour() != horaInicio) {
+			throw new BadRequestException("La fecha tiene que ser un lunes a la hora de apertura");
+		}
+
+		List<SlotDto> horarioAula = disponibilidadService.crearHorarioAula(idAula, inicioSemana);
+		return ResponseEntity.ok(horarioAula);
+		
+	}
+
 }
--- src/main/java/init/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/init/exceptions/handler/GlobalExceptionHandler.java
@@ -6,18 +6,24 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import init.exceptions.BadRequestException;
-import init.exceptions.InternalServerException;
+import init.exceptions.SlotNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
-	@ExceptionHandler
+	@ExceptionHandler(BadRequestException.class)
 	public ResponseEntity<String> handleBadRequestException(BadRequestException ex){
 		return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 	
-	@ExceptionHandler
-	public ResponseEntity<String> handleInternalServerException(InternalServerException ex){
+	@ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                              .body("Error al acceder a la base de datos.");
+    }
+	
+	@ExceptionHandler(SlotNotFoundException.class)
+	public ResponseEntity<String> handleSlotNotFoundException(SlotNotFoundException ex){
 		return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
