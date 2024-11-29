1eb704da78e384ab9ae567050ecb0ff5939fcb7b
Jorge Casas <j.casas.lopez.26@gmail.com>
2024-11-29 19:56:18 +0100

    DisponibilidadControllerCapaWebTest/crearHorarioAula_horarioActualizado (TEST)

--- src/test/java/init/DisponibilidadControllerCapaWebTest.java
+++ src/test/java/init/DisponibilidadControllerCapaWebTest.java
@@ -2,6 +2,7 @@ package init;
 
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -19,12 +20,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import init.config.ConfiguracionHoraria;
 import init.model.AulaDto;
+import init.model.SlotDto;
 import init.service.DisponibilidadService;
 
 @WebMvcTest
@@ -48,7 +51,7 @@ public class DisponibilidadControllerCapaWebTest {
 		return List.of(aula1);
 	}
 	
-	private RequestBuilder crearRequestBuilder(LocalDateTime inicioPeriodo, LocalDateTime finalPeriodo,
+	private RequestBuilder requestBuilderAulas(LocalDateTime inicioPeriodo, LocalDateTime finalPeriodo,
 			int capacidad, boolean proyector, boolean altavoces) {
 		return MockMvcRequestBuilders.get("/aulasDisponibles")
 				.param("horaInicio", inicioPeriodo.toString())  
@@ -58,16 +61,32 @@ public class DisponibilidadControllerCapaWebTest {
 				.param("altavoces", String.valueOf(altavoces))
 				.accept(MediaType.APPLICATION_JSON);
 	}
-
+	
+	private List<SlotDto> crearListaSlots() {
+		SlotDto slot1 = new SlotDto();
+		return List.of(slot1);
+	}
+	
+	private RequestBuilder requestBuilderHorario(int idAula, LocalDate inicioPeriodo,
+			LocalDate finalPeriodo) {
+		return MockMvcRequestBuilders.get("/crearHorarioAula")
+				.param("idAula", String.valueOf(idAula))
+				.param("inicioPeriodo", inicioPeriodo.toString())  
+				.param("finalPeriodo", finalPeriodo.toString())  
+				.accept(MediaType.APPLICATION_JSON);
+	}
+	
+	//TESTS PROPIAMENTE DICHOS
 	@Test
 	@DisplayName("Devuelve la lista de aulas disponibles")
 	void aulasDisponibles() throws Exception {
 		//Arrange
 		LocalDateTime inicioPeriodo = LocalDateTime.of(2024, 11, 11, 10, 0); 
-		LocalDateTime finalPeriodo = LocalDateTime.of(2024, 11, 11, 11, 0);
+		LocalDateTime finalPeriodo = inicioPeriodo.plusHours(1);
 		when(disponibilidadService.aulasDisponibles(inicioPeriodo, finalPeriodo, 10, false, false))
 											.thenReturn(crearListaAulas());
-		RequestBuilder requestBuilder = crearRequestBuilder(inicioPeriodo, finalPeriodo, 10, false, false);
+		RequestBuilder requestBuilder = requestBuilderAulas(inicioPeriodo, finalPeriodo, 
+				10, false, false);
 		
 		//Act
 		MvcResult mcvResult = mockMvc.perform(requestBuilder).andReturn();
@@ -92,7 +111,7 @@ public class DisponibilidadControllerCapaWebTest {
 	@CsvSource ({"12, 11",  
 		    "21, 21",   
 		    })
-	@DisplayName("Lanza excepción si el inicio no es anterior a la finalización")
+	@DisplayName("El inicio no puede ser anterior a la finalización")
 	void aulasDisponibles_lanzaExcepcionSiHorasNoSonCorrectas(int horaInicio, int horaFinal) throws Exception {
 		//Arrange
 		LocalDateTime inicioPeriodo = LocalDateTime.of(2024, 11, 11, horaInicio, 0);
@@ -100,7 +119,8 @@ public class DisponibilidadControllerCapaWebTest {
 		when(disponibilidadService.aulasDisponibles(inicioPeriodo, finalPeriodo, 10, false, false))
 													.thenReturn(crearListaAulas());
 				
-		RequestBuilder requestBuilder = crearRequestBuilder(inicioPeriodo, finalPeriodo, 10, false, false);
+		RequestBuilder requestBuilder = requestBuilderAulas(inicioPeriodo, finalPeriodo, 
+				10, false, false);
 				
 		//Act
 		MvcResult mcvResult = mockMvc.perform(requestBuilder).andReturn();
@@ -115,11 +135,12 @@ public class DisponibilidadControllerCapaWebTest {
 	void aulasDisponibles_lanzaExcepcionSiCapacidadNegativa() throws Exception {
 		//Arrange
 		LocalDateTime inicioPeriodo = LocalDateTime.of(2024, 11, 11, 11, 0);
-		LocalDateTime finalPeriodo = LocalDateTime.of(2024, 11, 11, 12, 0); 
+		LocalDateTime finalPeriodo = inicioPeriodo.plusHours(1);
 		when(disponibilidadService.aulasDisponibles(inicioPeriodo, finalPeriodo, -10, false, false))
 													.thenReturn(crearListaAulas());
 				
-		RequestBuilder requestBuilder = crearRequestBuilder(inicioPeriodo, finalPeriodo, -10, false, false);
+		RequestBuilder requestBuilder = requestBuilderAulas(inicioPeriodo, finalPeriodo, -10, 
+				false, false);
 				
 		//Act
 		MvcResult mcvResult = mockMvc.perform(requestBuilder).andReturn();
@@ -127,5 +148,31 @@ public class DisponibilidadControllerCapaWebTest {
 		//Assert
 		Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), mcvResult.getResponse().getStatus(), 
 				"El estatus HTTP devuelto es incorrecto");
+	}
+	
+	@Test
+	@DisplayName("Devuelve el horario actualizado del aula")
+	void crearHorarioAula_horarioActualizado() throws Exception{
+		//Arrange
+		int idAula = 1;
+		LocalDate inicioPeriodo = LocalDate.of(2024, 11, 11);
+		LocalDate finalPeriodo = inicioPeriodo.plusDays(1);		
+		when(disponibilidadService.crearHorarioAula(idAula, 
+				inicioPeriodo.atTime(configuracionHoraria.getHoraApertura(), 0), 
+				finalPeriodo.atTime(configuracionHoraria.getHoraCierre(), 0)))
+										.thenReturn(crearListaSlots());
+		RequestBuilder requestBuilder = requestBuilderHorario(idAula, inicioPeriodo, finalPeriodo);
+		
+		//Act
+		MvcResult mcvResult = mockMvc.perform(requestBuilder).andReturn();
+		
+		//Assert
+		String responseContent = mcvResult.getResponse().getContentAsString();
+		List<SlotDto> reponseSlots = objectMapper.readValue(responseContent, 
+				new TypeReference<List<SlotDto>>() {});
+		Assertions.assertEquals(HttpStatus.OK.value(), mcvResult.getResponse().getStatus(), 
+				"El estatus HTTP devuelto es incorrecto");
+		Assertions.assertEquals(1, reponseSlots.size(), 
+				"La lista no contiene el número de slots esperado");
 	}
 }
